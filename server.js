e9dcb041eb51e1865f214194a559221ffbc6e497
emmanuel24699 <egligbe246@gmail.com>
2026-03-11 09:48:33 +0000

    fix(deploy): use named parameter for catch-all route (Express 5 compat)

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Serve index.html for all other routes to support React Router
-app.get('(.*)', (req, res) => {
+app.get('/:path*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
